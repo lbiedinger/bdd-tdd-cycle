d7c2a5666ca7e4176a2421f6b260c58a593453ff
lutz <lutz.biedinger@gmail.com>
2014-06-10 11:47:32 +0100

    add git ignore and edit move page path cucumber helper

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -17,10 +17,8 @@ module NavigationHelpers
       '/'
 
     # Add more mappings here.
-    # Here is an example that pulls values out of the Regexp:
-    #
-    #   when /^(.*)'s profile page$/i
-    #     user_profile_path(User.find_by_login($1))
+    when (/^the edit page for "(.*)"$/)
+      edit_movie_path(Movie.find_by_title($1))
 
     else
       begin
