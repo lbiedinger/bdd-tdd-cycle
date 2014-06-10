ad67e02ad7065c37744a65c768484987f81586c5
lutz <lutz.biedinger@gmail.com>
2014-06-10 15:02:03 +0100

    added specs

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,6 +4,10 @@ Given (/^the following movies exist:$/) do |table|
   end
 end
 
+Given (/^(?:|I )am on the details page for "(.*?)"/) do |movie|
+  visit movie_path(Movie.find_by_title(movie))
+end
+
 Then (/the director of "(.*?)" should be "(.*?)"/) do |movie, director|
   expect(Movie.find_by_title(movie).director).to eq director
 end
--- rottenpotatoes/spec/requests/movie_pages_spec.rb
+++ rottenpotatoes/spec/requests/movie_pages_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe "Movie Pages" do
+  before {@movie = Movie.create()}
+
+  describe "edit pages" do
+    before {visit movie_edit_path(@movie)}
+    it "should have a director field" do
+
+    end
+  end
+
+  describe "details pages" do
+    before {visit movie_path(@movie)}
+    it "should have a 'Find Movies With Same Director' link" do
+      expect(page).to have_selector(:link_or_button, 'Find Movies With Same Director')
+    end
+  end
 end
